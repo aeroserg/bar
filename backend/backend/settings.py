17b70274a278081e5b7121d20fe592fe721d860b
losiik <49565548+losiik@users.noreply.github.com>
2023-11-06 02:36:37 +0400

    add cors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'administration',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -82,12 +84,7 @@ DATABASES = {
     }
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost",
-    'http://143.244.189.224',
-    "http://handmadebysemion.com",
-    "https://handmadebysemion.com"
-]
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Password validation
